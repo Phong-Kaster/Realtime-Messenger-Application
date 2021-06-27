4f0e102cb273eed09f89c22b08ba85a00de227a3
Phong-Kaster <phongkaster@gmail.com>
2021-06-27 14:41:30 +0700

    27-06-2021 try to ignore node module

--- app.js
+++ app.js
@@ -4,20 +4,7 @@ const app = express();
 const port = 3000;
 const connectDatabase = require('./config/connectDatabase.js');
 connectDatabase();
-const contactSchema = require('./models/contact.model');
-app.get('/createClone' , async (req,res) =>{
-    try {
-        let item = {
-            userId : "999",
-            contactId : "909"
-        }
-        let contact = await contactSchema.createClone( item);
-        res.send(contact);
-    }
-     catch (error) {
-        console.log( error);
-    }
-})
+
 
 app.listen( port, () => {
     console.log(`Server is running on port http://localhost/${port}`);
