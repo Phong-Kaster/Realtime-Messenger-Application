708e2cb23f841280e7793708c3bc6b1d027c8ba2
Phong-Kaster <phongkaster@gmail.com>
2021-07-05 19:36:47 +0700

    05-07-2021 update logo realtime messenger

--- sh/env.sh
+++
@@ -1,8 +0,0 @@
-#config database enviroment variable
-#env = environt variable
-export DB_CONNECTION=mongodb;
-export DB_HOST=localhost;
-export DB_PORT=27017;
-export DB_NAME=rma;
-export DB_USERNAME="";
-export DB_PASSWORD="";
