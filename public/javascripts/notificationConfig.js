3b7e0c07afdb0ea8c36a23a77df834cf14dbcc32
Phong-Kaster <phongkaster@gmail.com>
2021-07-22 17:58:38 +0700

    22-07-2021 optimize comment in notificationConfig.js

--- public/javascripts/notificationConfig.js
+++ public/javascripts/notificationConfig.js
@@ -1,3 +1,9 @@
+/*************************************************************
+ * @param {*} quantitySeenNotifications show total notification user have seen
+ * this function execute a AJAX request GET to retrieve older notifications
+ * old notifications will be appended continually.
+ * No more notification if  quantitySeenNotifications > 15
+ *************************************************************/
 let ajaxToRetrieveMoreOldNotification = (quantitySeenNotifications)=>{
     $.get(`/notification-read-more/${quantitySeenNotifications}`,function(oldNotifications){
             
@@ -9,27 +15,34 @@ let ajaxToRetrieveMoreOldNotification = (quantitySeenNotifications)=>{
             return false;
         }
 
-
         oldNotifications.forEach( (element)=>{
             $("ul.list-notifications").append(`<li>${element}</li>`);
-        })
+        });
+
+
         $("#btn-read-more-notifications").css("display","block");
         $(".lds-ring").css("display","none");
-
     });
 }
 
+
+
+/*************************************************************
+ * listen event click button "read more notification"
+ * count <li> tag as seen notifications
+ * sent AJAX request to load more older notifications
+ *************************************************************/
 $(document).ready(function(){
 
     $("#btn-read-more-notifications").bind("click",function()
     {
         let quantitySeenNotifications = $("ul.list-notifications").find("li").length;
-
-        // if(quantitySeenNotifications > 10)
-        // {
-        //     $("#btn-read-more-notifications").css("display","none");
-        //     $(".lds-ring").css("display","none");
-        // }
+        if(quantitySeenNotifications > 15)
+        {
+            $("#btn-read-more-notifications").css("display","none");
+            $(".lds-ring").css("display","none");
+            return false;
+        }
 
         $("#btn-read-more-notifications").css("display","none");
         $(".lds-ring").css("display","inline-block");
